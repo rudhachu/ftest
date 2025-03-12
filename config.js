7e349b22adc524b694265fc3f8eaf1293df063b0
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-13 00:30:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const toBool = x => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
 module.exports = {
  SESSION_ID: process.env.SESSION_ID || "",
- HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
+ HANDLERS: process.env.PREFIX === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
  BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", //YourName;BotName;Image/VideoLink
  SUDO: process.env.SUDO || "2348039607375,2349162411207",
  TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
@@ -22,7 +22,7 @@ module.exports = {
  AUTO_READ: toBool(process.env.AUTO_READ) || false,
  RMBG_KEY: process.env.RMBG_KEY || "",
  AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
- WORK_TYPE: process.env.WORK_TYPE || "private",
+ WORK_TYPE: process.env.MODE || "private",
  DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
  REMOVEBG: process.env.REMOVEBG || "",
